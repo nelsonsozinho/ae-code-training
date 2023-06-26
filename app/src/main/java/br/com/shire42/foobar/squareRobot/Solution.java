e5d7c02fde2e95860a29b267004fddb61dda3d02
Nelson Sozinho <nelsonsozinho@gmail.com>
2023-06-26 10:53:34 -0400

    init robot

---
+++ app/src/main/java/br/com/shire42/foobar/squareRobot/Solution.java
@@ -0,0 +1,9 @@
+package br.com.shire42.foobar.squareRobot;
+
+public class Solution {
+
+    public int solution(String[] input) {
+        return -1;
+    }
+
+}
---
+++ app/src/test/java/br/com/shire42/foobar/squareRobot/TestSolution.java
@@ -0,0 +1,25 @@
+package br.com.shire42.foobar.squareRobot;
+
+import org.junit.jupiter.api.Test;
+
+public class TestSolution {
+
+    private Solution solution;
+
+
+    @Test
+    public void setup() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void tetOne() {
+        String[][] input = new String[][]{
+                {".",".", ".", ".", ".", "."},
+                {".",".",".", ".", ".", "."}
+
+        };
+//        int result = solution.solution()
+    }
+
+}
